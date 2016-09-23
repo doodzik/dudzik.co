ffc35d82c06ac610d8734f68b5b8377f3152f87e
Frederik Dudzik <frederik@dudzik.co>
2016-09-23 21:07:44 +0200

    add browser and ip info

--- src/contact/send.php
+++ src/contact/send.php
@@ -8,9 +8,12 @@ function clean_string($string) {
 
 if (empty($_POST["username"]) && !empty($_POST["content"]) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
 
+  $ip      = $_SERVER['REMOTE_ADDR'];
+  $browser = $_SERVER['HTTP_USER_AGENT'];
+
   $subject = 'dudzik.co - form';
   $replyTo = $_POST["email"];
-  $message = clean_string($replyTo) . "\n\n" . clean_string($_POST["content"]);
+  $message = clean_string($replyTo) . "\n" . clean_string($ip) . "\n" . clean_string($browser) . "\n\n" . clean_string($_POST["content"]);
 
   $headers = "From: $from" . "\r\n" .
       "Reply-To: $replyTo" . "\r\n" .
